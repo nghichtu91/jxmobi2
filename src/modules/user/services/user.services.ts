701befb06a54d77ed743555d9192c0d629019d88
Your Name <nhatthanh5891@gmail.com>
2023-11-09 13:28:10 +0700

    update code

--- src/modules/user/services/user.services.ts
+++ src/modules/user/services/user.services.ts
@@ -49,7 +49,6 @@ export class UserService implements IUserService {
 
   getUser(userName: string): Promise<UserEntity> {
     return this.userRepository.findOne({
-
       where: {
         LoginName: userName,
       },

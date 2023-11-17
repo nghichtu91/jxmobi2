84467f6ffe503a44f2f6c030f08308f45b3d8b81
thanhpy <nhatthanh5891@gmail.com>
2023-11-17 16:28:46 +0700

    update code

--- src/modules/app/config/app-session.ts
+++ src/modules/app/config/app-session.ts
@@ -9,40 +9,44 @@ import { redisPort, redisUrl, sessionMaxAge, sessionSecret } from '@config';
 import { createClient } from 'redis';
 import connectRedis from 'connect-redis';
 
+const NODEENV = process.env.NODE_ENV || 'development';
+
 export function configSession(app: NestExpressApplication) {
-  // const RedisStore = connectRedis(session);
+  const config = {
+    // store: new RedisStore({ client: redisClient }),
+    resave: false,
+    saveUninitialized: false,
+    secret: sessionSecret,
+    cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: false, // if true prevent client side JS from reading the cookie
+      maxAge: parseDuration(sessionMaxAge, 'ms'), // session max age in milliseconds. Please restart Redis server after change this value!
+    },
+  } as session.SessionOptions;
 
-  // const redisClient = createClient({
-  //   url: `redis://${redisUrl}:${redisPort}`,
-  //   legacyMode: true,
-  // });
+  if (NODEENV === 'production') {
+    const RedisStore = connectRedis(session);
+    const redisClient = createClient({
+      url: `redis://${redisUrl}:${redisPort}`,
+      legacyMode: true,
+    });
 
-  // redisClient.on('error', (err) => {
-  //   Logger.log(
-  //     `Could not establish a connection with redis. ${err}`,
-  //     'Bootstrap',
-  //   );
-  // });
+    redisClient.on('error', (err) => {
+      Logger.log(
+        `Could not establish a connection with redis. ${err}`,
+        'Bootstrap',
+      );
+    });
 
-  // redisClient.connect();
+    redisClient.connect();
+    redisClient.on('connect', () => {
+      Logger.log(`Connected to redis successfully`, 'Bootstrap');
+    });
 
-  // redisClient.on('connect', () => {
-  //   Logger.log(`Connected to redis successfully`, 'Bootstrap');
-  // });
+    config.store = new RedisStore({ client: redisClient });
+  }
 
-  app.use(
-    session({
-      // store: new RedisStore({ client: redisClient }),
-      resave: false,
-      saveUninitialized: false,
-      secret: sessionSecret,
-      cookie: {
-        secure: false, // if true only transmit cookie over https
-        httpOnly: false, // if true prevent client side JS from reading the cookie
-        maxAge: parseDuration(sessionMaxAge, 'ms'), // session max age in milliseconds. Please restart Redis server after change this value!
-      },
-    }),
-  );
+  app.use(session(config));
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(flash());
